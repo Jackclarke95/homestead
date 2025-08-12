74f123a0b886e2cc994acf8438035f7283c32aad
Jack Clarke <jackclarke95@gmail.com>
2025-08-12 18:50:13 +0100

    Initial template stub

---
+++ src/main/java/jackclarke95/dryingrack/DryingRackDataGenerator.java
@@ -0,0 +1,11 @@
+package jackclarke95.dryingrack;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DryingRackDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
