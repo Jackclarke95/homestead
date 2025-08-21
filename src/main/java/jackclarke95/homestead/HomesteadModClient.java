edfc9b5c971b58f191426f74c2ffaca8e7eaa3e7
Jack Clarke <jackclarke95@gmail.com>
2025-08-21 20:09:04 +0100

    Initial setup

---
+++ src/main/java/jackclarke95/homestead/HomesteadModClient.java
@@ -0,0 +1,10 @@
+package jackclarke95.homestead;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HomesteadModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
