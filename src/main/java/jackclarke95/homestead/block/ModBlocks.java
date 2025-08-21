fc3d3b5fd2fa264b36830b81aeccf82e75e0802d
Jack Clarke <jackclarke95@gmail.com>
2025-08-21 22:36:36 +0100

    Crafting and loot tables

--- src/main/java/jackclarke95/homestead/block/ModBlocks.java
+++ src/main/java/jackclarke95/homestead/block/ModBlocks.java
@@ -4,21 +4,17 @@ import jackclarke95.homestead.Homestead;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
     public static final Block CURING_VAT = registerBlock("curing_vat",
-            new Block(AbstractBlock.Settings
-                    .create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.WOOD)));
+            new Block(AbstractBlock.Settings.copy(Blocks.CRAFTING_TABLE)));
 
     public static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
