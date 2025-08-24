14ea94e570d9f90934f2e5db167072ac0d7df22e
Jack Clarke <jackclarke95@gmail.com>
2025-08-25 00:57:45 +0100

    Update model and make transparent

--- src/main/java/jackclarke95/homestead/HomesteadClient.java
+++ src/main/java/jackclarke95/homestead/HomesteadClient.java
@@ -1,10 +1,16 @@
 package jackclarke95.homestead;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import jackclarke95.homestead.block.ModBlocks;
 
+@Environment(EnvType.CLIENT)
 public class HomesteadClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RACK, RenderLayer.getCutout());
     }
 }
