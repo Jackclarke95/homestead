dd69fd893c7da064279a18960a6f7e9f87caf754
Jack Clarke <jackclarke95@gmail.com>
2025-08-28 21:03:41 +0100

    Add basic feeding to animals within 8 blocks

--- src/main/java/jackclarke95/homestead/block/entity/custom/TroughBlockEntity.java
+++ src/main/java/jackclarke95/homestead/block/entity/custom/TroughBlockEntity.java
@@ -1,15 +1,19 @@
 package jackclarke95.homestead.block.entity.custom;
 
+import java.util.List;
+
 import jackclarke95.homestead.Homestead;
 import jackclarke95.homestead.block.entity.ImplementedInventory;
 import jackclarke95.homestead.block.entity.ModBlockEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 
 public class TroughBlockEntity extends BlockEntity implements ImplementedInventory {
     private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(1, ItemStack.EMPTY);
@@ -29,12 +33,31 @@ public class TroughBlockEntity extends BlockEntity implements ImplementedInvento
         // No custom data for now
     }
 
-    // Called every tick (server side)
     public void tick(net.minecraft.world.World world, BlockPos pos, BlockState state) {
-        // No logic for now
-        // every 20 ticks, do something
         if (world.getTime() % 100 == 0) {
-            Homestead.LOGGER.info("TroughBlockEntity ticking at " + pos);
+            Homestead.LOGGER.info("Attempting breeding");
+
+            attemptBreeding();
+        }
+    }
+
+    private void attemptBreeding() {
+        List<AnimalEntity> animals = world.getEntitiesByClass(
+                AnimalEntity.class,
+                new Box(pos).expand(8),
+                entity -> entity.isAlive() && !entity.isBaby() && !entity.isInLove() && entity.getBreedingAge() == 0);
+
+        if (animals.isEmpty()) {
+            Homestead.LOGGER.info("[Trough] No eligible animals found at " + pos);
+            return;
+        }
+
+        for (AnimalEntity animal : animals) {
+            Homestead.LOGGER.info("[Trough] Fed " + animal.getType().toString() + " at " + animal.getBlockPos()
+                    + " from trough at " + pos);
+            animal.lovePlayer(null);
+
+            break;
         }
     }
 }
