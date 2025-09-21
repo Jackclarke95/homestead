4f46a2d2a710e87a8af0219f0602c76f0fcf3094
Jack Clarke <jackclarke95@gmail.com>
2025-09-21 22:26:38 +0100

    Remove facing property from sowing block

--- src/main/java/jackclarke95/homestead/block/custom/SowingBedBlock.java
+++ src/main/java/jackclarke95/homestead/block/custom/SowingBedBlock.java
@@ -10,30 +10,24 @@ import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.state.StateManager;
-import net.minecraft.state.property.DirectionProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.player.PlayerEntity;
 import org.jetbrains.annotations.Nullable;
 
 public class SowingBedBlock extends BlockWithEntity {
-    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
     private static final MapCodec<SowingBedBlock> CODEC = createCodec(SowingBedBlock::new);
 
     public SowingBedBlock(Settings settings) {
         super(settings);
-        setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.NORTH));
+        setDefaultState(this.stateManager.getDefaultState());
     }
 
     @Override
@@ -53,18 +47,7 @@ public class SowingBedBlock extends BlockWithEntity {
     }
 
     @Override
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
-    }
-
-    @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
-    }
-
-    @Override
     protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        // Simple shallow bed shape
         return VoxelShapes.union(Block.createCuboidShape(0, 0, 0, 16, 16, 16));
     }
 
@@ -86,7 +69,7 @@ public class SowingBedBlock extends BlockWithEntity {
     @Override
     protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos,
             PlayerEntity player, Hand hand, BlockHitResult hit) {
-        // Default behaviour: open block entity screen if present
+
         if (!world.isClient) {
             BlockEntity be = world.getBlockEntity(pos);
             if (be instanceof SowingBedBlockEntity) {
