27ce06bb3cbe2b797fe8104a9350405d9a1c4988
Jack Clarke <jackclarke95@gmail.com>
2025-09-22 12:09:11 +0100

    Add remaining trees, and update rarity to 1%

--- src/main/java/jackclarke95/homestead/world/ModPlacedFeatures.java
+++ src/main/java/jackclarke95/homestead/world/ModPlacedFeatures.java
@@ -19,13 +19,45 @@ import net.minecraft.world.gen.placementmodifier.PlacementModifier;
 
 public class ModPlacedFeatures {
     public static final RegistryKey<PlacedFeature> PEAR_TREE_PLACED_KEY = registerKey("pear_tree_placed");
+    public static final RegistryKey<PlacedFeature> APPLE_TREE_PLACED_KEY = registerKey("apple_tree_placed");
+    public static final RegistryKey<PlacedFeature> PLUM_TREE_PLACED_KEY = registerKey("plum_tree_placed");
+    public static final RegistryKey<PlacedFeature> APRICOT_TREE_PLACED_KEY = registerKey("apricot_tree_placed");
+    public static final RegistryKey<PlacedFeature> PEACH_TREE_PLACED_KEY = registerKey("peach_tree_placed");
+    public static final RegistryKey<PlacedFeature> LEMON_TREE_PLACED_KEY = registerKey("lemon_tree_placed");
+    public static final RegistryKey<PlacedFeature> ORANGE_TREE_PLACED_KEY = registerKey("orange_tree_placed");
 
     public static void bootstrap(Registerable<PlacedFeature> context) {
         var configuredFeatures = context.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
 
         register(context, PEAR_TREE_PLACED_KEY, configuredFeatures.getOrThrow((ModConfiguredFeatures.PEAR_TREE_KEY)),
                 VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
-                        PlacedFeatures.createCountExtraModifier(2, 0.01f, 2), ModBlocks.PEAR_TREE_SAPLING));
+                        PlacedFeatures.createCountExtraModifier(0, 0.01F, 1), ModBlocks.PEAR_TREE_SAPLING));
+
+        register(context, APPLE_TREE_PLACED_KEY, configuredFeatures.getOrThrow((ModConfiguredFeatures.APPLE_TREE_KEY)),
+                VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
+                        PlacedFeatures.createCountExtraModifier(0, 0.01F, 1), ModBlocks.APPLE_TREE_SAPLING));
+
+        register(context, PLUM_TREE_PLACED_KEY, configuredFeatures.getOrThrow((ModConfiguredFeatures.PLUM_TREE_KEY)),
+                VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
+                        PlacedFeatures.createCountExtraModifier(0, 0.01F, 1), ModBlocks.PLUM_TREE_SAPLING));
+
+        register(context, APRICOT_TREE_PLACED_KEY,
+                configuredFeatures.getOrThrow((ModConfiguredFeatures.APRICOT_TREE_KEY)),
+                VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
+                        PlacedFeatures.createCountExtraModifier(0, 0.01F, 1), ModBlocks.APRICOT_TREE_SAPLING));
+
+        register(context, PEACH_TREE_PLACED_KEY, configuredFeatures.getOrThrow((ModConfiguredFeatures.PEACH_TREE_KEY)),
+                VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
+                        PlacedFeatures.createCountExtraModifier(0, 0.01F, 1), ModBlocks.PEACH_TREE_SAPLING));
+
+        register(context, LEMON_TREE_PLACED_KEY, configuredFeatures.getOrThrow((ModConfiguredFeatures.LEMON_TREE_KEY)),
+                VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
+                        PlacedFeatures.createCountExtraModifier(0, 0.01F, 1), ModBlocks.LEMON_TREE_SAPLING));
+
+        register(context, ORANGE_TREE_PLACED_KEY,
+                configuredFeatures.getOrThrow((ModConfiguredFeatures.ORANGE_TREE_KEY)),
+                VegetationPlacedFeatures.treeModifiersWithWouldSurvive(
+                        PlacedFeatures.createCountExtraModifier(0, 0.01F, 1), ModBlocks.ORANGE_TREE_SAPLING));
     }
 
     public static RegistryKey<PlacedFeature> registerKey(String name) {
--- src/main/java/jackclarke95/homestead/world/gen/ModTreeGenerations.java
+++ src/main/java/jackclarke95/homestead/world/gen/ModTreeGenerations.java
@@ -8,9 +8,34 @@ import net.minecraft.world.gen.GenerationStep;
 
 public class ModTreeGenerations {
     public static void generateTrees() {
-        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST),
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.PLAINS),
                 GenerationStep.Feature.VEGETAL_DECORATION,
                 ModPlacedFeatures.PEAR_TREE_PLACED_KEY);
-    }
 
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.PLAINS),
+                GenerationStep.Feature.VEGETAL_DECORATION,
+                ModPlacedFeatures.APPLE_TREE_PLACED_KEY);
+
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.PLAINS),
+                GenerationStep.Feature.VEGETAL_DECORATION,
+                ModPlacedFeatures.PLUM_TREE_PLACED_KEY);
+
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.PLAINS),
+                GenerationStep.Feature.VEGETAL_DECORATION,
+                ModPlacedFeatures.APRICOT_TREE_PLACED_KEY);
+
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.PLAINS),
+                GenerationStep.Feature.VEGETAL_DECORATION,
+                ModPlacedFeatures.PEACH_TREE_PLACED_KEY);
+
+        BiomeModifications.addFeature(
+                BiomeSelectors.includeByKey(BiomeKeys.SAVANNA, BiomeKeys.WINDSWEPT_SAVANNA, BiomeKeys.SAVANNA_PLATEAU),
+                GenerationStep.Feature.VEGETAL_DECORATION,
+                ModPlacedFeatures.LEMON_TREE_PLACED_KEY);
+
+        BiomeModifications.addFeature(
+                BiomeSelectors.includeByKey(BiomeKeys.SAVANNA, BiomeKeys.WINDSWEPT_SAVANNA, BiomeKeys.SAVANNA_PLATEAU),
+                GenerationStep.Feature.VEGETAL_DECORATION,
+                ModPlacedFeatures.ORANGE_TREE_PLACED_KEY);
+    }
 }
